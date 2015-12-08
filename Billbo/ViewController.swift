018bd7c5d13fea121885eb15cdfaef42a3d715f6
kuocrystal <ck78214@hotmail.com>
2015-12-08 13:48:32 +0800

    Billbo_iOS
    617 Billbo_iOS

---
+++ Billbo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Billbo
+//
+//  Created by Crystal Kuo on 11/2/15.
+//  Copyright © 2015 Crystal Kuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://crystalkuo214.com/617/billbo_a/index.html");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.loadRequest(requestObj);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
